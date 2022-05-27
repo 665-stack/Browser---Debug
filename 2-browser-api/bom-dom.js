99354d02d72b60694376b5b85c97430ba22416bf
unknown <mahmodulshahin665@gmail.com>
2022-05-27 11:21:59 +0600

    explore bom, dom and browser api

---
+++ 2-browser-api/bom-dom.js
@@ -0,0 +1,17 @@
+/* 
+BOM => Browser object model
+DOM => Document object model
+
+akhane amra jodi html file add kora cara ba browser er shate js code er add kora carai dom ba fetch use kori taile error dibe. ar ei js file er shathe browser er add korle jeshob shobida pawya jay takei Browser Api/ Web Api bole.
+*/
+const name = 15;
+const student = {
+    name: 'Jolil',
+    friend: 'Borsha',
+    job: 'Businees',
+    makeMovie: function (name) {
+        console.log(name);
+    }
+}
+student.makeMovie('Din The Day')
+document.getElementById('name')
