d9b7f954033bed8cdbecdbf48be7517fcb3c8961
unknown <mahmodulshahin665@gmail.com>
2022-05-30 12:31:43 +0600

    debug and solution

---
+++ 2-browser-api/summary.js
@@ -0,0 +1,9 @@
+const itsAlert = () => {
+    alert('Alert! You are going to be a Developer.')
+}
+const itsConfirm = () => {
+    confirm('Are you single?')
+}
+const itsPrompt = () => {
+    prompt("Then what's your name")
+}
