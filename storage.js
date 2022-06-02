820b99d1835703496745331dca26d357f56fa200
unknown <mahmodulshahin665@gmail.com>
2022-06-02 09:41:06 +0600

    complete practice 5

---
+++ storage.js
@@ -0,0 +1,6 @@
+/* 
+What is the different between local storage and session storage ?
+==> Local storage has data for very long time.
+==> Session storage has date when I'm browsing website.
+==> Session storage is similar to Local storage. The difference is that while data in localStorage doesn't expire, data in sessionStorage is cleared when the page session ends.
+*/
