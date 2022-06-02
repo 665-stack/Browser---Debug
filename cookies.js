089a875ec2faa91e1ab4d44eb0c6e4d4f8147200
unknown <mahmodulshahin665@gmail.com>
2022-06-02 08:50:45 +0600

    complete practice 4

---
+++ cookies.js
@@ -0,0 +1,12 @@
+/* 
+What is Cookies ? 
+==> A cookie is information that a website puts on a user's computer. 
+==> They also sometimes referred to as browser cookies, web cookies or internet cookies.
+==> Common use cases for cookies include session management, personalization and tracking.
+==> Cookies in the cyber world come in two types: session and persistent.
+==> Session cookies are used only while navigating a website. They are stored in random access memory and are never written to the hard drive. 
+==> Persistent cookies remain on a computer indefinitely, although many include an expiration date and are automatically removed when that date is reached.
+==> Cookies have been part of daily internet operations for decades and are generally safe. However, third-party cookies are sometimes seen as intrusive.
+
+
+*/
