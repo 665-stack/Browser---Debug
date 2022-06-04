522a99ab6524021869dd418a1e6f7a7b20a52e45
unknown <mahmodulshahin665@gmail.com>
2022-06-05 02:55:42 +0600

    add style and load api data

--- conceptual-session/singer-song/script.js
+++ conceptual-session/singer-song/script.js
@@ -1,1 +1,26 @@
-console.log('d');
+
+const elementById = (id) => {
+    const inputField = document.getElementById(id);
+    return inputField;
+
+}
+const handleSearch = () => {
+    const keyword = elementById('keyword');
+    const url = `https://www.theaudiodb.com/api/v1/json/2/search.php?s=${keyword.value}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => showArtist(data.artists))
+
+}
+const showArtist = (data) => {
+    const artistsContainer = elementById('artists')
+    data.forEach(artist => {
+        console.log(artist);
+        const div = document.createElement('div');
+        div.innerHTML = `
+           <p>${artist.strArtist}</p>
+         `;
+        artistsContainer.appendChild(div)
+    });
+}
+
