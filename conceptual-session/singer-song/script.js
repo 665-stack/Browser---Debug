cb42fd2f67ff0e00e1f0cb1e920425505e0a75fa
unknown <mahmodulshahin665@gmail.com>
2022-06-05 13:55:19 +0600

    add style and load album api data

--- conceptual-session/singer-song/script.js
+++ conceptual-session/singer-song/script.js
@@ -1,26 +1,69 @@
-
 const elementById = (id) => {
-    const inputField = document.getElementById(id);
-    return inputField;
+    const inputfield = document.getElementById(id);
+    return inputfield;
+};
 
-}
 const handleSearch = () => {
-    const keyword = elementById('keyword');
+    const keyword = elementById("keyword");
     const url = `https://www.theaudiodb.com/api/v1/json/2/search.php?s=${keyword.value}`;
     fetch(url)
-        .then(res => res.json())
-        .then(data => showArtist(data.artists))
+        .then((res) => res.json())
+        .then((data) => showArtists(data));
+    keyword.value = '';
+};
 
-}
-const showArtist = (data) => {
-    const artistsContainer = elementById('artists')
-    data.forEach(artist => {
-        console.log(artist);
-        const div = document.createElement('div');
-        div.innerHTML = `
-           <p>${artist.strArtist}</p>
-         `;
-        artistsContainer.appendChild(div)
+const showArtists = (data) => {
+    const artistContainer = elementById("artists");
+    data?.artists?.forEach((artist) => {
+        const div = document.createElement("div");
+        div.classList.add("artist-card");
+        div.innerHTML = `<div class="image-container">
+      <div class="image-container-inner">
+        <img
+          src="${artist.strArtistThumb}"
+          alt=""
+        />
+      </div>
+    </div>
+    <div class="info-container">
+      <h1>${artist.strArtist}</h1>
+      <p>Country: ${artist.strCountry}</p>
+      <p>Style: ${artist.strGenre}</p>
+    </div>
+    <button onclick="fetchAlbums('${artist.idArtist}')" class="album-button">
+      <i class="fa-solid fa-compact-disc"></i>
+      <p  class="button-title">Albums</p>
+    </button>`;
+        artistContainer.appendChild(div);
     });
-}
+};
 
+const fetchAlbums = (id) => {
+    const url = `https://theaudiodb.com/api/v1/json/2/album.php?i=${id}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => showAlbum(data.album));
+    const artistContainer = elementById("artists");
+    artistContainer.innerHTML = "";
+};
+
+const showAlbum = (data) => {
+    const albumContainer = elementById("albums");
+    data.forEach((album) => {
+        const div = document.createElement("div");
+        div.classList.add("album");
+        div.innerHTML = `
+          <div class="album-image-container">
+            <img
+              src="${album.strAlbumThumb}"
+              alt=""
+            />
+          </div>
+          <div class="album-name">
+            <h3>${album.strAlbum}</h3>
+          </div>
+        `;
+
+        albumContainer.appendChild(div);
+    });
+};
