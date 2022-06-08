0e1a74b6736037e2739f05b2ede7e24eeda84918
unknown <mahmodulshahin665@gmail.com>
2022-06-08 10:38:26 +0600

    code for quiz

---
+++ practice/quiz.js
@@ -0,0 +1,15 @@
+const unique = array => array.indexOf('21')
+
+const array = [2, 3, 4, 5, 2];
+const not = unique(array)
+console.log(not)
+//===========================
+const add = (para1, para2) => para2 + para1;
+const addto = add('2', 3);
+console.log(addto);
+//================================
+/* const b = 6, b = 6
+console.log(b); */
+//====================
+const colors = { mango: 'green', grapes: 'black', organe: 'yellow' };
+console.log(colors[grapes])
