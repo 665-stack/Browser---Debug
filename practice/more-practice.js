a255bef188879aaacaf80e449c9308c43ecd6b71
unknown <mahmodulshahin665@gmail.com>
2022-06-08 21:39:44 +0600

    practice object destructuring

---
+++ practice/more-practice.js
@@ -0,0 +1,41 @@
+arr = () => {
+    return 9;
+}
+console.log(arr());
+//====================
+arrTwo = num => {
+    return num * 12;
+}
+console.log(arrTwo(10));
+//===========================
+arrThree = (num1, num2) => {
+    const sumNum = num1 + num2;
+    const divide = sumNum / 4;
+    return divide;
+}
+console.log(arrThree(9, 11))
+//============================
+arrFour = (num1, num2) => {
+    const sum1 = num1 + 5;
+    const sum2 = num2 + 5;
+    const multiply = sum1 * sum2;
+    return multiply;
+}
+console.log(arrFour(5, 5));
+//======================
+const user = {
+    name: 'mahmodul hasan shahin',
+    salary: 20000,
+    position: "web developer"
+}
+const firstNmae = user.name
+console.log(firstNmae);
+//==========================
+const customer = {
+    name: 'Mahmodul Shahin',
+    age: 18,
+    profession: 'Web Developer'
+}
+const { name, profession, age } = customer;
+console.log(name, age);
+//====================
